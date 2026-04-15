4e6aed07948b31a33d7b9acf5352e5be29aab75e
Christoph Gahabka <christoph.gahabka@web.de>
2026-04-15 09:52:04 +0200

    project init
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/de/dhbwravensburg/webeng/pilotlogbook/PilotLogbookApplication.java
@@ -0,0 +1,13 @@
+package de.dhbwravensburg.webeng.pilotlogbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilotLogbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PilotLogbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/dhbwravensburg/webeng/pilotlogbook/PilotLogbookApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dhbwravensburg.webeng.pilotlogbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PilotLogbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
